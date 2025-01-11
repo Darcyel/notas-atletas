040981ab1cbcfebac4048cff4ec259739d0d2b74
Darcyel <darcyelalves17@gmail.com>
2025-01-10 23:10:35 -0300

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //Porjeto 01 - Pontuação dos atletas
-class notasDosAtletas {
-  constructor (atletas) {
+class NotasDosAtletas {
+  constructor(atletas) {
     this.atletas = atletas;
   }
   organizarNotas() {
     this.atletas.forEach(function(atleta) {
-      atleta.notas = atletas.notas.sort(function(a, b) {
+      atleta.notas = atleta.notas.sort(function(a, b) {
         return a - b;
       })
     })
@@ -32,5 +32,44 @@ class notasDosAtletas {
       this.media.push(this.soma[i] / this.notas[i].length);
     }
   }
-  
-      
+  resultado() {
+    let resultado = ``;
+    for (let i = 0; i < this.atletas.length; i++) {
+      resultado = resultado + `Atleta: ${this.atletas[i].nome}
+    Notas Obtidas: ${this.atletas[i].notas.join(",")}
+    Notas Válidas: ${this.media[i].toFixed(2)}
+    ======================
+  `;
+    }
+    console.log(resultado);
+  }
+
+}
+
+let atletas = [
+  {
+    nome: "Cesar Abascal",
+    notas: [10, 9.34, 8.42, 10, 7.88]
+  },
+  {
+    nome: "Fernando Puntel",
+    notas: [8, 10, 10, 7, 9.33]
+  },
+  {
+    nome: "Daiane Jelinsky",
+    notas: [7, 10, 9.5, 9.5, 8]
+  },
+  {
+    nome: "Bruno Castro",
+    notas: [10, 10, 10, 9, 9.5]
+  }
+  ];
+
+let notasDosAtletas = new NotasDosAtletas(atletas);
+notasDosAtletas.organizarNotas();
+notasDosAtletas.excluirNotas();
+notasDosAtletas.somarNotas();
+notasDosAtletas.mediaDasNotas();
+notasDosAtletas.resultado()
+
+
